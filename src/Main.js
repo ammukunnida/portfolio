a7ee2a5c807759f87b09c60fddc9c8f01a7c571d
Anaswara M A <anaswaram21@yahoo.com>
2024-10-29 14:13:39 +0530

    Initial Commit

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <Router>
       <Routes>
-        {/* The Routes decides which component to show based on the current URL. */}
+        {/* The Routes decides which component to show based on the current url. */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
